6484179a86c24ed43264bd03043792ec05f98037
awood976 <awood976@yahoo.com>
2020-07-30 23:21:56 -0700

    Many Updates!
    Set page Title, Icon. Added Resume file. Moved button functionality and various css to external files. Added scrolling effect to background. Added content to profile.

---
+++ scripts/buttonfunctions.js
@@ -0,0 +1,12 @@
+function mobileWindow() {
+  myWindow = window.open("https://awood976.github.io/index.html","","width=550,height=800");
+}
+
+function toggleRainEffect(elem) {
+  var element = document.getElementById(elem);
+  if(element.classList.contains("motion-scroll")) {
+    element.classList.remove("motion-scroll");
+  } else {
+    element.classList.add("motion-scroll");    
+  }
+}
